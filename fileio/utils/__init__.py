5bab83065a996d2cb4481899919680bded0d5aae
trisongz <tri@songzcorp.com>
2021-05-13 01:50:43 -0500

    fx

--- fileio/utils/__init__.py
+++ fileio/utils/__init__.py
@@ -6,10 +6,12 @@ logger = get_logger()
 _enable_pbar = False
 
 from . import ops
-from . import ds
-from . import multi
-
-
 from .ops import Auth, lazy_import, exec_command, lazy_install
+
+from . import multi
 from .multi import MultiThreadPipeline
+
+from . import ds
 from .ds import TFDSIODataset
+
+
